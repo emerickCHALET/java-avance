bdde0748ae022586f6080a2d6619b02902af327e
laurentsem <laurent.sem@edu.itescia.Fr>
2021-01-20 11:19:01 +0100

    Doing Story 1...

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -5,8 +5,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Launcher extends Application {
+
+    private static int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
+    }
+
 
     public static void main(String[] args){ launch(args);}
 
@@ -23,5 +46,30 @@ public class Launcher extends Application {
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+
+        // Conversion d'une l'image vers le binaire
+        TFUtils tf = new TFUtils();
+        BufferedImage bi = ImageIO.read(new File("/Users/laurentsem/Documents/Projects/java-avance/src/main/resources/inception5h/tensorPics/jack.jpg"));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bi, "jpg", baos);
+        byte[] imgData = baos.toByteArray();
+        System.out.println(imgData);
+        // Conversion du binaire vers Tensor
+        Tensor toTensor = tf.byteBufferToTensor(imgData);
+        System.out.println("To sensor :" + toTensor);
+
+        // Conversion de toutes les images
+        //byte[] allImg = Files.readAllBytes(Paths.get("/Users/laurentsem/Documents/Projects/java-avance/src/main/resources/inception5h/tensorPics"));
+        byte[] graphDef = Files.readAllBytes(Paths.get("/Users/laurentsem/Documents/Projects/java-avance/src/main/resources/inception5h/tensorflow_inception_graph.pb"));
+        List<String> labels = Files.readAllLines(Paths.get("/Users/laurentsem/Documents/Projects/java-avance/src/main/resources/inception5h/labels.txt"));
+
+
+        Tensor probs = tf.executeModelFromByteArray(graphDef, toTensor);
+        System.out.println("Probs: " + probs);
+
+
+        // Pour exécuter le modèle afin de sortir les probs
+
+
     }
 }
--- src/main/java/launcher/TFUtils.java
+++ src/main/java/launcher/TFUtils.java
@@ -1,3 +1,5 @@
+package launcher;
+
 import org.tensorflow.*;
 
 import java.io.IOException;
@@ -24,6 +26,9 @@ public class TFUtils {
             throw new Error("invalid path");
         }
     }
+
+    //
+
 
     Tensor executeModelFromByteArray(byte[] graphDef, Tensor input) {
         try (Graph g = new Graph()) {
