fd057e181c68878fa7b59775a8d30afa2993b446
houssam <hlaghzil@gmail.com>
2021-01-20 15:36:42 +0100

    Story 2 class

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -3,6 +3,8 @@ package launcher;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.tensorflow.Graph;
@@ -23,15 +25,7 @@ import java.util.List;
 public class Launcher extends Application {
 
     //
-    private static int maxIndex(float[] probabilities) {
-        int best = 0;
-        for (int i = 1; i < probabilities.length; ++i) {
-            if (probabilities[i] > probabilities[best]) {
-                best = i;
-            }
-        }
-        return best;
-    }
+
 
     public static void main(String[] args) {
         launch(args);
@@ -39,17 +33,30 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hello World!");
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((action) -> {
-            System.out.println("Hello World!");
+        TextField txt = new TextField();
+        //Image img = new Image();
+        Button btns = new Button();
+        Story1 story1 = new Story1();
+        btns.setText("Story1");
+        //txt.setText(Story1.storyOne());
+        btns.setOnAction((action) -> {
+            try {
+                System.out.println(Story1.storyOne());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
+
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
+        root.getChildren().add(btns);
+        root.getChildren().add(txt);
         primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();*/
-        Story1 story1 = new Story1();
-        story1.storyOne();
+        primaryStage.show();
+
+
     }
 }
--- src/main/java/launcher/Story1.java
+++ src/main/java/launcher/Story1.java
@@ -11,52 +11,59 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-public class Story1
-{
-    public static float[] storyOne() throws Exception
-    {
+public class Story1 {
+    private static int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
+    }
+
+    public static Story1 storyOne() throws Exception {
 
         // Conversion d'une l'image vers le binaire
         TFUtils tf = new TFUtils();
 
-        File repertoire = new File("../java-avance/src/main/resources/inception5h/tensorPics");
-        File[] files = repertoire.listFiles();
-
-        for (File f: files) {
-            System.out.println("File" + f);
-
-            BufferedImage bi = ImageIO.read(new File(String.valueOf(f)));
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(bi, "jpg", baos);
-            byte[] imgData = baos.toByteArray();
-            System.out.println(imgData);
-            // Conversion du binaire vers Tensor
-            Tensor toTensor = tf.byteBufferToTensor(imgData);
-            System.out.println("To sensor :" + toTensor);
+        BufferedImage bi = ImageIO.read(new File("../java-avance/src/main/resources/inception5h/tensorPics/keyboard.jpg"));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bi, "jpg", baos);
+        byte[] imgData = baos.toByteArray();
+        System.out.println(imgData);
+        // Conversion du binaire vers Tensor
+        Tensor toTensor = tf.byteBufferToTensor(imgData);
+        System.out.println("To sensor :" + toTensor);
 
 
-            // Graph + label
-            byte[] graphDef = Files.readAllBytes(Paths.get("../java-avance/src/main/resources/inception5h/tensorflow_inception_graph.pb"));
-            List<String> labels = Files.readAllLines(Paths.get("../java-avance/src/main/resources/inception5h/labels.txt"));
+        // Graph + label
+        byte[] graphDef = Files.readAllBytes(Paths.get("../java-avance/src/main/resources/inception5h/tensorflow_inception_graph.pb"));
+        List<String> labels = Files.readAllLines(Paths.get("../java-avance/src/main/resources/inception5h/labels.txt"));
 
 
-            // Probs
-            Tensor<Float> probs = tf.executeModelFromByteArray(graphDef, toTensor);
-            System.out.println("Probs: " + probs);
-            // Result : [1, 1008]
+        // Probs
+        Tensor<Float> probs = tf.executeModelFromByteArray(graphDef, toTensor);
+        System.out.println("Probs: " + probs);
+        // Result : [1, 1008]
 
-            // Affichage prédiction
-            final long[] rshape = probs.shape();
-            if (probs.numDimensions() != 2 || rshape[0] != 1) {
-                throw new RuntimeException(
-                        String.format(
-                                "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
-                                Arrays.toString(rshape)));
-            }
-            int nlabels = (int) rshape[1];
-            float[] resultProbs = probs.copyTo(new float[1][nlabels])[0];
-            return resultProbs;
+        // Affichage prédiction
+        final long[] rshape = probs.shape();
+        if (probs.numDimensions() != 2 || rshape[0] != 1) {
+            throw new RuntimeException(
+                    String.format(
+                            "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+                            Arrays.toString(rshape)));
         }
-        return null;
+        int nlabels = (int) rshape[1];
+        float[] resultProbs = probs.copyTo(new float[1][nlabels])[0];
+        int bestLabelIdx = maxIndex(resultProbs);
+        System.out.println(String.format("BEST MATCH: %s (%.2f%% likely)",
+                labels.get(bestLabelIdx),
+                resultProbs[bestLabelIdx] * 100f));
+        String resultLabel= String.format("BEST MATCH: %s (%.2f%% likely)",
+                labels.get(bestLabelIdx),
+                resultProbs[bestLabelIdx] * 100f);
+        return new Story1();
     }
 }
